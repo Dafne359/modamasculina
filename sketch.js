48f8417cbb63bf912f882331d5446ff1583bf91d
Dafne359 <126804063+Dafne359@users.noreply.github.com>
2023-06-15 20:24:16 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,4 +4,29 @@ function setup() {
 
 function draw() {
   background(220);
+
+  // Dibujar una camisa
+  fill(255, 0, 0); // Color rojo
+  rect(150, 150, 100, 150);
+
+  // Dibujar pantalones
+  fill(0, 0, 255); // Color azul
+  rect(150, 300, 100, 100);
+
+  // Dibujar zapatos
+  fill(0); // Color negro
+  rect(135, 400, 130, 30);
+
+  // Dibujar cabeza
+  fill(255, 204, 153); // Color piel
+  ellipse(200, 100, 100, 100);
+
+  // Dibujar ojos
+  fill(255); // Color blanco
+  ellipse(180, 90, 20, 20);
+  ellipse(220, 90, 20, 20);
+
+  // Dibujar boca
+  fill(0); // Color negro
+  rect(180, 120, 40, 10);
 }
